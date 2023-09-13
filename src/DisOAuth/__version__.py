76b7e635746b7599ddbe9060dade5f6f70455e49
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-13 09:36:50 -0500

    Update __version__.py

--- src/DisOAuth/__version__.py
+++ src/DisOAuth/__version__.py
@@ -1,4 +1,4 @@
 __title__ = "DisOAuth"
-__description__ = "Discord OAuth is now easy"
-__version__ = "0.0.1"
+__description__ = "Discord OAuth is now easier"
+__version__ = "0.1.0"
 __author__ = "Arcader717"
