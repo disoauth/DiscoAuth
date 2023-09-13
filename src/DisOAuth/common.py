537518c29d8df7ecaf42727fa21211bd540d57ee
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-13 09:33:11 -0500

    Update common.py
    re-updated the compliance with PEP 8, using flake8

--- src/DisOAuth/common.py
+++ src/DisOAuth/common.py
@@ -1,9 +1,10 @@
 from secrets import SystemRandom
 
-UNICODE_ASCII_CHARACTER_SET = ("abcdefghijklmnopqrstuvwxyz"
-                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                              "0123456789")
+UNICODE_ASCII_CHARACTER_SET = ('abcdefghijklmnopqrstuvwxyz'
+                               'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                               '0123456789')
+
 
 def generate_token(length=30, chars=UNICODE_ASCII_CHARACTER_SET):
     rand = SystemRandom()
-    return "".join(rand.choice(chars) for x in range(length))
+    return ''.join(rand.choice(chars) for x in range(length))
