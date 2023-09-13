97ccd5f1b57d38c01b11357b81ccfb8d5152a189
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-13 09:26:38 -0500

    Update url.py
    Added compliance with PEP 8 at:
    peps.python.org/pep-0008/

--- src/DisOAuth/url.py
+++ src/DisOAuth/url.py
@@ -1,30 +1,35 @@
 from typing import List
 
-import requests
 from .common import generate_token
 
 
 class AuthUrl:
-  """
-  
-  """
+    """"""
 
-  def __init__(self, client_id: str, scope: List[str], redirect_uri: str) -> None:
-    
-    self.baseUrl = "https://discord.com/oauth2/authorize?response_type=code&"
-    self.client_id = "client_id=" + client_id + "&"
-    x = 0
-    strScope = ""
-    while x < len(scope):
-      strScope = strScope + scope[x] + "%20"
-      x += 1
-    self.scope = "scope=" + strScope[:-3] 
-    redRep1 = redirect_uri.replace(":", "%3A")
-    redRep2 = redRep1.replace("/", "%2F")
-    redRep3 = redRep2.replace("-", "%2D")
-    self.redirect_uri = "redirect_uri=" + redRep3
-    self.state = "state=" + generate_token()
-    self.url = self.baseUrl + self.client_id + self.redirect_uri + "&" + self.scope + "&" + self.state
+    def __init__(self, client_id: str,
+                 scope: List[str],
+                 redirect_uri: str) -> None:
 
-  def __str__(self) -> str:
-    return self.url
+        self.bUrl = "https://discord.com/oauth2/authorize?response_type=code&"
+        self.client_id = "client_id=" + client_id + "&"
+        x = 0
+        strScope = ""
+        while x < len(scope):
+            strScope = strScope + scope[x] + "%20"
+        x += 1
+        self.scope = "scope=" + strScope[:-3]
+        redRep1 = redirect_uri.replace(":", "%3A")
+        redRep2 = redRep1.replace("/", "%2F")
+        redRep3 = redRep2.replace("-", "%2D")
+        self.redirect_uri = "redirect_uri=" + redRep3
+        self.state = "state=" + generate_token()
+        self.url = (self.bUrl
+                    + self.client_id
+                    + self.redirect_uri
+                    + "&"
+                    + self.scope
+                    + "&"
+                    + self.state)
+
+    def __str__(self) -> str:
+        return self.url
