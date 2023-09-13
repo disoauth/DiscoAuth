777f6030f85dbdef050e834934f74deaac9b1761
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-13 09:52:18 -0500

    Update common.py
    Made it so the unicode characters couldn't be called on

--- src/DisOAuth/common.py
+++ src/DisOAuth/common.py
@@ -1,10 +1,10 @@
 from secrets import SystemRandom
 
-UNICODE_ASCII_CHARACTER_SET = ('abcdefghijklmnopqrstuvwxyz'
+_UNICODE_ASCII_CHARACTER_SET = ('abcdefghijklmnopqrstuvwxyz'
                                'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                                '0123456789')
 
 
-def generate_token(length=30, chars=UNICODE_ASCII_CHARACTER_SET):
-    rand = SystemRandom()
-    return ''.join(rand.choice(chars) for x in range(length))
+def generate_token(length=30, chars=_UNICODE_ASCII_CHARACTER_SET):
+    _rand = SystemRandom()
+    return ''.join(_rand.choice(chars) for x in range(length))
