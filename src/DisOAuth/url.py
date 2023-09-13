c71e2e14ee12bf001f97343bc71f2e9b08d066cf
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-13 09:53:09 -0500

    Update url.py
    Added a docstring to __init__() of the AuthUrl class

--- src/DisOAuth/url.py
+++ src/DisOAuth/url.py
@@ -4,32 +4,37 @@ from .common import generate_token
 
 
 class AuthUrl:
-    """"""
 
     def __init__(self, client_id: str,
                  scope: List[str],
                  redirect_uri: str) -> None:
+        """Makes and returns a url that is used to authorize users
 
-        self.bUrl = "https://discord.com/oauth2/authorize?response_type=code&"
-        self.client_id = "client_id=" + client_id + "&"
-        x = 0
-        strScope = ""
-        while x < len(scope):
-            strScope = strScope + scope[x] + "%20"
-        x += 1
-        self.scope = "scope=" + strScope[:-3]
-        redRep1 = redirect_uri.replace(":", "%3A")
-        redRep2 = redRep1.replace("/", "%2F")
-        redRep3 = redRep2.replace("-", "%2D")
-        self.redirect_uri = "redirect_uri=" + redRep3
-        self.state = "state=" + generate_token()
-        self.url = (self.bUrl
-                    + self.client_id
-                    + self.redirect_uri
+        Keyword arguments:
+        client_id -- The client ID of your discord app
+        scope -- A list of scopes you want to use
+        redirect_uri -- The redirect uri you want to use
+        """
+        self._bUrl = "https://discord.com/oauth2/authorize?response_type=code&"
+        self._client_id = "client_id=" + client_id + "&"
+        _x = 0
+        _strScope = ""
+        while _x < len(scope):
+            _strScope = _strScope + scope[_x] + "%20"
+        _x += 1
+        self._scope = "scope=" + _strScope[:-3]
+        _redRep1 = redirect_uri.replace(":", "%3A")
+        _redRep2 = _redRep1.replace("/", "%2F")
+        _redRep3 = _redRep2.replace("-", "%2D")
+        self._redirect_uri = "redirect_uri=" + _redRep3
+        self._state = "state=" + generate_token()
+        self.url = (self._bUrl
+                    + self._client_id
+                    + self._redirect_uri
                     + "&"
-                    + self.scope
+                    + self._scope
                     + "&"
-                    + self.state)
+                    + self._state)
 
     def __str__(self) -> str:
         return self.url
