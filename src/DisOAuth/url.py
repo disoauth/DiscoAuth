52c60072086bf3d950cb868cef86e6730c7b1308
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-15 17:24:02 -0500

    Update url.py
    Added:
    The class 'discordApi'
    The functionality to get the access token
    to be able to check your auth info using an access token

--- src/DisOAuth/url.py
+++ src/DisOAuth/url.py
@@ -1,10 +1,14 @@
+import json
 from typing import List
 
-from .common import generate_token
+import requests
+
+from .common import generate_token, getToken
+
+apiUrl = "https://discord.com/api"
 
 
 class AuthUrl:
-
     def __init__(self, client_id: str,
                  scope: List[str],
                  redirect_uri: str) -> None:
@@ -38,3 +42,52 @@ class AuthUrl:
 
     def __str__(self) -> str:
         return self.url
+
+
+class discordApi:
+
+    def __init__(self,
+                 client_id,
+                 client_secret,
+                 scope: list[str],
+                 redirect_uri) -> None:
+        """Where you can get an access code, use the api links to get info
+
+        client_id - The client id of your application
+        client_secret - The client secret of your application
+        scope - a list of the scopes you authorized for
+        redirect_uri -- the redirect_uri you want to use
+        """
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.scope = scope
+        self.redirect_uri = redirect_uri
+
+    def accessToken(self, code) -> dict[str, str]:
+        """Takes values input into discordApi, and the code
+        returns the response as a dictionary
+
+        Keyword Arguments:
+        code -- The code you got when the user authorized
+        """
+        tokenDict = getToken(code,
+                             self.scope,
+                             self.redirect_uri,
+                             self.client_id,
+                             self.client_secret)
+        return tokenDict
+
+    def checkAuthInfo(self, token):
+        """requests your auth info for your application, returning it as a dict
+
+        If you want to get the full application object, use checkAppInfo()
+
+        Keyword Arguments:
+        token -- An access token, used for authorization"""
+        url = apiUrl + "/oauth2/@me"
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        r = requests.get(url,
+                         headers=headers,
+                         auth=token)
+        if r.status_code == 200:
+            return json.loads(str(r.json))
