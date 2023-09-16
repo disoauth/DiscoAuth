81f760d9c5db99c536fad19eb5ab61a6666b0311
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-15 21:51:30 -0500

    Update common.py

--- src/DisOAuth/common.py
+++ src/DisOAuth/common.py
@@ -1,10 +1,37 @@
+import json
 from secrets import SystemRandom
 
-_UNICODE_ASCII_CHARACTER_SET = ('abcdefghijklmnopqrstuvwxyz'
-                               'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                               '0123456789')
+import requests
+
+_UNICODE_ASCII_CHARACTER_SET = ('abcdefghijklmnopqrstuvwxyz',
+                                'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                                '0123456789')
 
 
 def generate_token(length=30, chars=_UNICODE_ASCII_CHARACTER_SET):
     _rand = SystemRandom()
     return ''.join(_rand.choice(chars) for x in range(length))
+
+
+def getToken(code: str,
+             scope: list[str],
+             redirect_uri: str,
+             client_id: str,
+             client_secret: str) -> dict[str, str]:
+    url = "https://discord.com/api/oauth2/token"
+    scopeStr = " ".join(scope)
+    rDict: dict[str, str] = {"error": "nothing loaded"}
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': redirect_uri,
+        'scope': scopeStr
+    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    r = requests.post(url=url,
+                      data=data,
+                      headers=headers,
+                      auth=(client_id, client_secret))
+    if r.status_code == 200:
+        rDict = json.loads(str(r.json))
+    return rDict  # returns the json file
