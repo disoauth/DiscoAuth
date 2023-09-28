2eb0cf55107b392f26db5c9d05a31003937b27d6
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-28 13:36:43 -0500

    Update url.py
    Built a UserObj class, or User Object
    
    Just had to put this down

--- src/DisOAuth/url.py
+++ src/DisOAuth/url.py
@@ -1,5 +1,4 @@
-import json
-from typing import List
+from typing import List, Optional, Union
 
 import requests
 
@@ -79,6 +78,8 @@ class discordApi:
         def __init__(self, access_token):
             self.access_token = access_token
 
+        
+
         async def get_current_user(self):
             url = apiUrl + "/users/@me"
             headers = {
@@ -86,4 +87,81 @@ class discordApi:
                 'Authorization': 'Bearer ' + self.access_token
             }
             r = requests.get(url, headers=headers)
+            j = r.json()
+            return UserObj(j['id'],
+                           j['username'],
+                           j['discriminator'], 
+                           j['global_name'],
+                           j['avatar'],
+                           j['bot'],
+                           j['system'],
+                           j['mfa_enabled'],
+                           j['banner'],
+                           j['accent_color'],
+                           j['locale'],
+                           j['verified'],
+                           j['email'],
+                           j['flags'],
+                           j['premium_type'],
+                           j['public_flags'],
+                           j['avatar_decoration'])
+
+        async def get_user_guilds(self):
+            url = apiUrl + "/users/@me/guilds"
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + self.access_token
+            }
+            r = requests.get(url, headers=headers)
             return r.json()
+            
+class UserObj:
+            def __init__(self,
+                         id,
+                         username: str,
+                         discriminator: str,
+                         global_name: Union[str,None],
+                         avatar: Optional[str],
+                         bot: Optional[bool],
+                         system: Optional[bool],
+                         mfa_enabled: Optional[bool],
+                         banner: Optional[str],
+                         accent_color: Optional[int],
+                         locale: Optional[str],
+                         verified: Optional[bool],
+                         email: Optional[str],
+                         flags: Optional[int],
+                         premium_type: Optional[int],
+                         public_flags: Optional[int],
+                         avatar_decoration: Optional[str]) -> None:
+                self.id = id
+                self.username = username
+                self.discriminator = discriminator
+                if global_name is not None:
+                    self.global_name = global_name
+                if avatar is not None:
+                    self.avatar = avatar
+                if bot is not None:
+                    self.bot = bot
+                else:
+                    self.bot = False
+                if system is not None:
+                    self.system = system
+                else:
+                    self.system = False
+                self.mfa_enabled = mfa_enabled
+                self.banner = banner
+                self.accent_color = accent_color
+                self.locale = locale
+                if verified is not None:
+                    self.verified = verified
+                else:
+                    self.verified = None
+                if email is not None:
+                    self.email = email
+                else:
+                    self.email = None
+                self.flags = flags
+                self.premium_type = premium_type
+                self.public_flags = public_flags
+                self.avatar_decoration = avatar_decoration
