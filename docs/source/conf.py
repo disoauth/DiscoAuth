0d8c0e2e8aaa7e83fa09e484e46b8d5d9894dd88
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-10-22 16:36:33 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,10 @@ project = 'DisOAuth2'
 copyright = '2023, Arcader717'
 author = 'Arcader717'
 release = 'v1.0'
-extensions = []
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.autosummary'
+]
 templates_path = ['_templates']
 excludes_patterns = []
 html_theme = 'alabaster'
