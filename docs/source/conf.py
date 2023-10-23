76269684835cb26ca1a39f122407c704ed255575
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-10-23 11:05:04 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,3 +9,6 @@ extensions = [
 excludes_patterns = []
 html_theme = 'furo'
 html_static_path = ['_static']
+html_theme_options = {
+  "source_repository": "https://github.com/Arcader717/Async-DisOAuth2"
+}
