2a853e85a1ea8cf6024c1f078dffc017897f655c
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-10-27 08:39:02 -0500

    Update models.py

--- src/DisOAuth/models.py
+++ src/DisOAuth/models.py
@@ -6,33 +6,33 @@ class UserObj:
         self.username = r['username']
         self.discriminator = r['discriminator']
         if 'global_name' in r:
-            self.global_name = r.global_name
+            self.global_name = r['global_name']
         if 'avatar' in r:
-            self.avatar = r.avatar
+            self.avatar = r['avatar']
         if 'bot' in r:
-            self.bot = r.bot
+            self.bot = r['bot']
         else:
             self.bot = False
         if 'system' in r:
-            self.system = r.system
+            self.system = r['system
         else:
             self.system = False
-        self.mfa_enabled = r.mfa_enabled
-        self.banner = r.banner
-        self.accent_color = r.accent_color
-        self.locale = r.locale
+        self.mfa_enabled = r['mfa_enabled']
+        self.banner = r['banner']
+        self.accent_color = r['accent_color']
+        self.locale = r['locale']
         if 'verified' in r:
-            self.verified = r.verified
+            self.verified = r['verified']
         else:
             self.verified = None
         if 'email' in r:
-            self.email = r.email
+            self.email = r['email']
         else:
             self.email = None
-        self.flags = r.flags
-        self.premium_type = r.premium_type
-        self.public_flags = r.public_flags
-        self.avatar_decoration = r.avatar_decoration
+        self.flags = r['flags']
+        self.premium_type = r['premium_type']
+        self.public_flags = r['public_flags']
+        self.avatar_decoration = r['avatar_decoration']
 
     def __getitem__ (self, key):
         return getattr(self, key)
