2deb5290847a72a82c33bd12638942d8e3fabd0c
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-08 18:34:57 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,5 +17,5 @@ html_theme = 'furo'
 html_static_path = ['_static']
 html_theme_options = {
   "source_repository": "https://github.com/Arcader717/Async-DisOAuth2",
-  "announcement": "DisOAuth will be renamed to discoauth after the 2.0 update"
+  "announcement": "DisOAuth will be renamed to discoauth after the 2.0 update, please checkout the <a href="https://async-disoauth2.readthedocs.io/en/cli/depreciate.html>Depreciation</a> documentation"
 }
