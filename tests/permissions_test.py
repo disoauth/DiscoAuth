6b01915646b5354c653fbff33b43d9219ab54a63
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-10 14:26:22 -0600

    Update permissions_test.py

--- tests/permissions_test.py
+++ tests/permissions_test.py
@@ -1,45 +1,35 @@
 from DisOAuth import permissions
 
-def test_int():
-    perms = permissions(3)
-    assert perms.value == 8
+def check(perms, value: int | None = None):
+    assert perms.value == value
     assert perms.administrator is True
     assert perms.stream is False
 
+
+def test_int():
+    perms = permissions(3)
+    check(perms, 8)
+
 def test_str():
     perms = permissions("administrator")
-    assert perms.administrator is True
-    assert perms.stream is False
-    assert perms.value == 8
+    check(perms, 8)
 
 def test_list_int():
     perms = permissions([3, 2, 1])
     assert perms.administrator is True
-    assert perms.ban_members is True
-    assert perms.kick_members is True
     assert perms.stream is False
-    assert perms.connect is False
-    assert perms.use_vad is False
     assert perms.value == 14
 
 def test_list_str():
     perms = permissions(["administrator", "ban_members", "kick_members"])
     assert perms.administrator is True
-    assert perms.ban_members is True
-    assert perms.kick_members is True
     assert perms.stream is False
-    assert perms.connect is False
-    assert perms.use_vad is False
     assert perms.value == 14
 
 def test_list_mix():
     perms = permissions([3, "ban_members", 1])
     assert perms.administrator is True
-    assert perms.ban_members is True
-    assert perms.kick_members is True
     assert perms.stream is False
-    assert perms.connect is False
-    assert perms.use_vad is False
     assert perms.value == 14
 
 def test_dict_int():
@@ -56,4 +46,22 @@ def test_dict_str():
     assert perms.kick_members is True
     assert perms.value == 10
 
-def test__up_itn
+def test_up_int():
+    perms = permissions()
+    assert perms.value == 0
+    perms.update(3)
+    assert perms.value == 8
+    assert perms.administrator is True
+    assert perms.stream is False
+
+
+def test_up_str():
+    perms = permissions()
+    perms.update("administrator")
+    assert perms.value == 8
+    assert perms.administrator is True
+    assert perms.stream is False
+
+def test_up_listInt():
+    perms = permissions()
+    perms.update([3, 2, 1])
