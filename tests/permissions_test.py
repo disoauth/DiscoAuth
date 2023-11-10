b56a336f4d3bd3fcb3c8d53a3fa79055a0810b68
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-10 19:08:06 +0000

    Update permissions_test.py
    Also moved the docs to their correct spot, according to the cli branch

--- tests/permissions_test.py
+++ tests/permissions_test.py
@@ -3,49 +3,57 @@ from DisOAuth import permissions
 def test_int():
     perms = permissions(3)
     assert perms.value == 8
-    assert perms.administrator == True
-    assert perms.stream == False
+    assert perms.administrator is True
+    assert perms.stream is False
 
 def test_str():
     perms = permissions("administrator")
-    assert perms.administrator == True
-    assert perms.stream == False
+    assert perms.administrator is True
+    assert perms.stream is False
     assert perms.value == 8
 
 def test_list_int():
     perms = permissions([3, 2, 1])
-    assert perms.administrator == True
-    assert perms.ban_members == True
-    assert perms.kick_members == True
-    assert perms.stream == False
-    assert perms.connect == False
-    assert perms.use_vad == False
+    assert perms.administrator is True
+    assert perms.ban_members is True
+    assert perms.kick_members is True
+    assert perms.stream is False
+    assert perms.connect is False
+    assert perms.use_vad is False
     assert perms.value == 14
 
 def test_list_str():
     perms = permissions(["administrator", "ban_members", "kick_members"])
-    assert perms.administrator == True
-    assert perms.ban_members == True
-    assert perms.kick_members == True
-    assert perms.stream == False
-    assert perms.connect == False
-    assert perms.use_vad == False
+    assert perms.administrator is True
+    assert perms.ban_members is True
+    assert perms.kick_members is True
+    assert perms.stream is False
+    assert perms.connect is False
+    assert perms.use_vad is False
     assert perms.value == 14
 
 def test_list_mix():
     perms = permissions([3, "ban_members", 1])
-    assert perms.administrator == True
-    assert perms.ban_members == True
-    assert perms.kick_members == True
-    assert perms.stream == False
-    assert perms.connect == False
-    assert perms.use_vad == False
+    assert perms.administrator is True
+    assert perms.ban_members is True
+    assert perms.kick_members is True
+    assert perms.stream is False
+    assert perms.connect is False
+    assert perms.use_vad is False
     assert perms.value == 14
 
 def test_dict_int():
     perms = permissions({3: True, 2: False, 1: True})
+    assert perms.administrator is True
+    assert perms.ban_members is False
+    assert perms.kick_members is True
     assert perms.value == 10
 
 def test_dict_str():
     perms = permissions({"administrator": True, "ban_members": False, "kick_members": True})
+    assert perms.administrator is True
+    assert perms.ban_members is False
+    assert perms.kick_members is True
     assert perms.value == 10
+
+def test__up_itn
