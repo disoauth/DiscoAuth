a5fb1d98306412c16d8959bdc7d040e99e35f590
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-13 07:50:14 -0600

    Apply suggestions from code review
    Removed the cli import into __init__

--- src/DisOAuth/__init__.py
+++ src/DisOAuth/__init__.py
@@ -1,5 +1,3 @@
 from .url import auth as AuthUrl, discord as discordApi, bot, permissions
 
 from .__version__ import __title__, __version__, __description__, __author__
-
-from .__main__ import cli
