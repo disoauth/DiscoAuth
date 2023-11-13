eddf4d9e1c9e5233b06e909c2c74ac7365a97f72
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-13 09:49:12 -0600

    fix auth_test.py

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -3,10 +3,10 @@ from DisOAuth import auth, permissions
 TEST_DIS_APP_ID = "1172638888553103371"
 
 async def test_url_build():
-    url = await auth(TEST_DIS_APP_ID, ["identify", "guilds"], "https://tests.com", test=True).makeUrl()
+    url = await auth(TEST_DIS_APP_ID, ["identify", "guilds"], "https://tests.com", test=True).url()
     assert url == "https://discord.com/oauth2/authorize?response_type=code&client_id=1172638888553103371&scope=identify%20guilds&state=1&redirect_uri=https%3A%2F%2Ftests.com"
 
 async def test_perm_build():
     perms = permissions(3) # Check permissions_test.py for the permissions tests
-    url = await auth(TEST_DIS_APP_ID, ["identify", "guilds", "bot"], "https://tests.com", perms, test=True).makeUrl()
+    url = await auth(TEST_DIS_APP_ID, ["identify", "guilds", "bot"], "https://tests.com", perms, test=True).url()
     assert url == "https://discord.com/oauth2/authorize?response_type=code&client_id=1172638888553103371&scope=identify%20guilds%20bot&state=1&redirect_uri=https%3A%2F%2Ftests.com&permissions=8"
