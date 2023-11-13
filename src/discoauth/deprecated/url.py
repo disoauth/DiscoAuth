a68de74dfa3279cf265375a0a5f980372c9b0b6b
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-13 13:25:57 -0600

    Update url.py

--- src/discoauth/deprecated/url.py
+++ src/discoauth/deprecated/url.py
@@ -2,9 +2,9 @@ from typing import Any, List, Type, Dict
 
 import requests
 
-from DisOAuth.deprecated.common import generate_token, getToken, htmlEncode, joinUrl, permsByList, bot_perms, bot_perms_key
+from discoauth.deprecated.common import generate_token, getToken, htmlEncode, joinUrl, permsByList, bot_perms, bot_perms_key
 
-from DisOAuth.deprecated.models import UserObj as uObj, GuildObj as gObj
+from discoauth.deprecated.models import UserObj as uObj, GuildObj as gObj
 
 apiUrl = "https://discord.com/api"
 
