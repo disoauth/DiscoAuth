6f372c02f6b8a5e05b97aea7f23f674e5f290ec2
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-14 16:09:41 -0600

    Update api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -2,5 +2,5 @@ from discoauth import auth, discord
 
 # if it doesn't make sense, check the workflow for testing. The workflow adds variables, to keep the environment variables secret
 
-async def test_make_api()
+async def test_make_api():
   api = discord(clid, clsec, ["identify"], "https://tests.com")
