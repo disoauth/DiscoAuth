50221b70818f4bf6f686476170dd7c927d0bf6a2
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-14 10:34:16 -0600

    Update Dauth_test.py

--- tests/Dauth_test.py
+++ tests/Dauth_test.py
@@ -11,8 +11,3 @@ async def test_perm_build():
     perms = permissions(3) # Check permissions_test.py for the permissions tests
     url = await AuthUrl(TEST_DIS_APP_ID, ["identify", "guilds", "bot"], "https://tests.com", perms, test=True).makeUrl()
     assert url == "https://discord.com/oauth2/authorize?response_type=code&client_id=1172638888553103371&scope=identify%20guilds%20bot&state=1&redirect_uri=https%3A%2F%2Ftests.com&permissions=8"
-
-async def test_perm_build():
-    perms = permissions(3) # Check permissions_test.py for the permissions tests
-    url = await AuthUrl(TEST_DIS_APP_ID, ["identify", "guilds", "bot"], "https://tests.com", perms.value, test=True).makeUrl()
-    assert url == "https://discord.com/oauth2/authorize?response_type=code&client_id=1172638888553103371&scope=identify%20guilds%20bot&state=1&redirect_uri=https%3A%2F%2Ftests.com&permissions=8"
