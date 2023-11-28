e966eeded1f3a6ae859c2bf20f50c0d1ed724e91
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-28 07:33:31 -0600

    Fix api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -3,11 +3,11 @@ from discoauth import auth, discord
 # if it doesn't make sense, check the workflow for testing. The workflow adds variables, to keep the environment variables secret
 
 async def test_make_api():
-    api = discord(clid, clsec, ["identify"], "https://example.com")
+    api = discord(clid, clsec, ["identify"], "https://www.example.com")
     token = await api.token(code)
 
 async def test_get_user():
-    api = discord(clid, clsec, ["identify"], "https://example.com")
+    api = discord(clid, clsec, ["identify"], "https://www.example.com")
     token = await api.token(code)
     userAPI = api.user(token)
     user1 = await userAPI.fetch(clid)
